142b1a5dedb1d97f108585779808978b83ef61b7
Barry Russell <blrussell09@gmail.com>
2018-07-07 10:52:46 -0500

    Added route for Home Page

--- src/app/App.js
+++ src/app/App.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import './App.css';
-import Fish from '../components/Fish/Fish.js';
+
+// import Fish from '../components/Fish/Fish.js';
 import Home from '../components/Home/Home.js';
-import Inventory from '../components/Inventory/Inventory.js';
-import Login from '../components/Login/Login.js';
+// import Inventory from '../components/Inventory/Inventory.js';
+// import Login from '../components/Login/Login.js';
 import Navbar from '../components/Navbar/Navbar.js';
-import New from '../components/New/New.js';
-import Order from '../components/Order/Order.js';
-import OrderSpa from '../components/OrderSpa/OrderSpa.js';
-import Register from '../components/Register/Register.js';
-import SingleOrder from '../components/SingleOrder/SingleOrder.js';
+// import New from '../components/New/New.js';
+// import Order from '../components/Order/Order.js';
+// import OrderSpa from '../components/OrderSpa/OrderSpa.js';
+// import Register from '../components/Register/Register.js';
+// import SingleOrder from '../components/SingleOrder/SingleOrder.js';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <Fish/>
-        <Home/>
-        <Inventory/>
-        <Login/>
-        <Navbar/>
-        <New/>
-        <Order/>
-        <OrderSpa/>
-        <Register/>
-        <SingleOrder/>
+        <BrowserRouter>
+          <div>
+            <Navbar/>
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
