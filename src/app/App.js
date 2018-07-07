e402b4a90feed3ab9bb6c197f0b767f4a0a92798
Barry Russell <blrussell09@gmail.com>
2018-07-07 10:23:27 -0500

    Added components

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import Fish from '../components/Fish/Fish.js';
+import Home from '../components/Home/Home.js';
+import Inventory from '../components/Inventory/Inventory.js';
+import Login from '../components/Login/Login.js';
+import Navbar from '../components/Navbar/Navbar.js';
+import New from '../components/New/New.js';
+import Order from '../components/Order/Order.js';
+import OrderSpa from '../components/OrderSpa/OrderSpa.js';
+import Register from '../components/Register/Register.js';
+import SingleOrder from '../components/SingleOrder/SingleOrder.js';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-danger">YP</button>
+        <Fish/>
+        <Home/>
+        <Inventory/>
+        <Login/>
+        <Navbar/>
+        <New/>
+        <Order/>
+        <OrderSpa/>
+        <Register/>
+        <SingleOrder/>
       </div>
     );
   }
---
+++ src/components/SingleOrder/SingleOrder.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SingleOrder.css';
+
+class SingleOrder extends React.Component
+{
+  render ()
+  {
+    return (
+      <div className="SingleOrder">
+        <h1>Single Order</h1>
+      </div>
+    );
+  }
+};
+
+export default SingleOrder;
