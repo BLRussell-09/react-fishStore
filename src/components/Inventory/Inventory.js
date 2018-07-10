e7b5e8088db4b73a72082dbda8fe5fae8d086547
Barry Russell <blrussell09@gmail.com>
2018-07-09 19:26:13 -0500

    finished get requests and am now displaying fishes on inventory page

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import './Inventory.css';
+import fishRequests from '../../firebaseRequests/fishes';
 
 class Inventory extends React.Component
 {
+
+  state =
+  {
+    fishes: [],
+  };
+
+  componentDidMount ()
+  {
+    fishRequests
+      .getRequest()
+      .then((fishes) =>
+      {
+        this.setState({fishes});
+      })
+      .catch((err) =>
+      {
+        console.error('There was an error getting fish' ,err);
+      });
+  };
+
   render ()
   {
+
+    const fishComponents = this.state.fishes.map((fish) =>
+    {
+      return (
+        <h2>{fish.name}</h2>
+      );
+    });
+
     return (
       <div className="Inventory">
         <h1>Inventory</h1>
+        <ul className="fishes">
+          {fishComponents}
+        </ul>
       </div>
     );
   }
