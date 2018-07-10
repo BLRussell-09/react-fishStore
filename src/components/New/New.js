944563fc66a1b6812e63979c5584b1e7cf198d85
Barry Russell <blrussell09@gmail.com>
2018-07-09 21:15:55 -0500

    Added inventory and new page with orders

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import Fish from '../Fish/Fish';
+import fishRequests from '../../firebaseRequests/fishes';
+import Order from '../Order/Order';
 import './New.css';
 
 class New extends React.Component
 {
+
+  state =
+  {
+    fishes: [],
+    order: {},
+  };
+
+  componentDidMount ()
+  {
+    fishRequests
+      .getRequest()
+      .then((fishes) =>
+      {
+        this.setState({fishes});
+      })
+      .catch((err) =>
+      {
+        console.error('There was an error getting fish' ,err);
+      });
+  }
+
   render ()
   {
+
+    const fishComponents = this.state.fishes.map((fish) =>
+    {
+      return (
+        <Fish
+          details={fish}
+          key={fish.id}
+        />
+      );
+    });
+
     return (
       <div className="New">
-        <h1>New</h1>
+        <div className="col-xs-8 inventory-container">
+          <h2>Inventory</h2>
+          <ul className="fishes">
+            {fishComponents}
+          </ul>
+        </div>
+        <Order />
       </div>
     );
   }
